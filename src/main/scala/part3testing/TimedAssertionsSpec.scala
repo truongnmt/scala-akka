55532705cd067f0c9cc163b108718de0b40cb35e
Truong Nguyen <truong.nguyen@paypay-corp.co.jp>
2024-05-06 22:39:32 +0900

    Timed Assertions

---
+++ src/main/scala/part3testing/TimedAssertionsSpec.scala
@@ -0,0 +1,87 @@
+package part3testing
+
+import akka.actor.{Actor, ActorSystem, Props}
+import akka.testkit.{ImplicitSender, TestKit, TestProbe}
+import com.typesafe.config.ConfigFactory
+import org.scalatest.{BeforeAndAfterAll}
+import org.scalatest.wordspec.AnyWordSpecLike
+
+import scala.concurrent.duration._
+import scala.util.Random
+import scala.language.postfixOps
+
+/*
+  Problem 1: in practice, we have a lot of actors that take a lot of time to response to a query for a large chunk of work
+  For example, doing hard computations or waiting for a resources or things like that
+
+  2. There are actors which reply to a work request in a rapid fire succession of smaller chunks of work
+
+  No influence to test probe
+ */
+
+class TimedAssertionsSpec extends TestKit(
+  ActorSystem("TimedAssertionsSpec", ConfigFactory.load().getConfig("specialTimedAssertionsConfig")))
+  with ImplicitSender
+  with AnyWordSpecLike
+  with BeforeAndAfterAll {
+
+  override def afterAll(): Unit = {
+    TestKit.shutdownActorSystem(system)
+  }
+
+  import TimedAssertionsSpec._
+
+  "A worker actor" should {
+    val workerActor = system.actorOf(Props[WorkerActor])
+
+    "reply with the meaning of life in a timely manner" in {
+      within(500 millis, 1 second) { // at most 1 second
+        workerActor ! "work"
+        expectMsg(WorkResult(42))
+      }
+    }
+
+    "reply with valid work at a reasonable cadence" in {
+      within(1 second) {
+        workerActor ! "workSequence"
+
+        val results: Seq[Int] = receiveWhile[Int](max=2 seconds, idle=500 millis, messages=4) {
+          case WorkResult(result) => result
+        }
+
+        assert(results.sum > 5)
+      }
+    }
+
+    "reply to a test probe in a timely manner" in {
+      within(1 second) {
+        val probe = TestProbe()
+        probe.send(workerActor, "work")
+        probe.expectMsg(WorkResult(42)) // timeout of 0.3 seconds
+      }
+    }
+
+  }
+
+}
+
+object TimedAssertionsSpec {
+
+  case class WorkResult(result: Int)
+
+  class WorkerActor extends Actor {
+    override def receive: Receive = {
+      case "work" =>
+        // long computation
+        Thread.sleep(500)
+        sender() ! WorkResult(42)
+
+      case "workSequence" =>
+        val r = new Random()
+        for (_ <- 1 to 10) {
+          Thread.sleep(r.nextInt(50))
+          sender() ! WorkResult(1)
+        }
+    }
+  }
+}
